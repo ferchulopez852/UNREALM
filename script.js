76d1974c6ddebf0f352d4c24993fa0dc66c1b517
ferchulopez852 <ferchulopezriver@gmail.com>
2026-06-24 15:10:14 -0300

    Agregado buscador y mejoras visuales

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log("Portfolio iniciado");
+
+const buscador = document.getElementById("searchInput");
+
+if(buscador){
+
+    buscador.addEventListener("keyup", () => {
+
+        let filtro = buscador.value.toLowerCase();
+
+        let tarjetas = document.querySelectorAll(".game-card");
+
+        tarjetas.forEach(card => {
+
+            let texto = card.innerText.toLowerCase();
+
+            if(texto.includes(filtro)){
+
+                card.style.display = "block";
+
+            }else{
+
+                card.style.display = "none";
+
+            }
+
+        });
+
+    });
+
+}
